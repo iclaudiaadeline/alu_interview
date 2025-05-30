8db938118bff455b405af83fdf76a87ef92437c6
iclaudiaadeline <a.iradukun3@alustudent.com>
2025-05-30 07:22:06 +0200

    alu interview

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Minimum Operations """
+
+
+def minOperations(n):
+    """Return the minimum number of operations to get n H characters"""
+    if n < 2:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while factor * factor <= n:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    if n > 1:
+        operations += n
+
+    return operations
